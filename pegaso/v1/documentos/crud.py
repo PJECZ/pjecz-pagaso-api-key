44560c032eec93b4f08eb547de5dbf7772680991
RicVal <none@none.com>
2023-03-15 15:53:59 -0600

    cambios

--- pegaso/v1/documentos/crud.py
+++ pegaso/v1/documentos/crud.py
@@ -15,20 +15,25 @@ def get_documento_from_num_expediente(
     num_expediente: str,
 ) -> Documento:
     """Consultar un documento por su número de expediente"""
+
     # Validar el juzgado_id
     if juzgado_id <= 0:
         raise PegasoNotValidParamError("No es válido el número del juzgado")
+
     # Validar el número de expediente
     try:
         num_expediente = safe_expediente(num_expediente)
     except (ValueError, IndexError) as err:
         raise PegasoNotValidParamError("No es válido el número de expediente")
-
     if num_expediente is None or num_expediente == "":
         raise PegasoNotValidParamError("No es válido el número de expediente")
+
+    # Consultar el documento
     documento = db.query(Documento).filter_by(expediente=num_expediente).filter_by(juzgado_id=juzgado_id).first()
     if documento is None:
         raise PegasoNotExistsError("No existe ese documento")
     if documento.estatus != "A":
         raise PegasoIsDeletedError("No es un documento activo, está eliminado")
+
+    # Entregar
     return documento
